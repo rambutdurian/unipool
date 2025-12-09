6ee8bfab1c07961c3e899707e1c914486083ca74
Mazzy45 <nurmaziah45@gmail.com>
2025-12-09 22:16:42 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'start_page.dart';
-import 'home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:month_year_picker/month_year_picker.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,6 +16,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+      // Required for month_year_picker
+      localizationsDelegates: const [
+        MonthYearPickerLocalizations.delegate,
+      ],
 
       // Start screen first
       home: const StartPage(),
